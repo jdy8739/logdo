f7ebfa93093fe2f97841cc18f4ff1ad643741580
jdy8739 <jdy8739@naver.com>
2025-03-27 22:50:29 +0900

    style: set tablet responsive in Footer

--- src/components/common/Footer.css.ts
+++ src/components/common/Footer.css.ts
@@ -1,13 +1,21 @@
 import { style } from '@vanilla-extract/css';
+import { responsiveStyle } from '../../styles/responseive.css';
 
-const footer = style({
-  display: 'grid',
-  placeItems: 'center',
-  marginTop: 'auto',
-  padding: '50px 0',
-  fontSize: '15px',
-  textAlign: 'center',
-  lineHeight: '1.5',
-});
+const footer = style([
+  {
+    display: 'grid',
+    placeItems: 'center',
+    marginTop: 'auto',
+    padding: '50px 0',
+    fontSize: '15px',
+    textAlign: 'center',
+    lineHeight: '1.5',
+  },
+  responsiveStyle({
+    tablet: {
+      fontSize: '12px',
+    },
+  }),
+]);
 
 export { footer };
