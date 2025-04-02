2d982771b3d1a082e47f4294dd94cb6b76ac3c19
jdy8739 <jdy8739@naver.com>
2025-04-02 17:33:52 +0900

    refac: replace div tags to semantic tags and wrap PostItem with the Link tag

--- src/components/main/PostItem.tsx
+++ src/components/main/PostItem.tsx
@@ -7,40 +7,42 @@ import {
   postCategory,
 } from './PostItem.css';
 import { PostItem as PostItemType } from '../../types/type';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby';
+import { formatDate } from '../utils/dateUtils';
 
 const PostItem = ({
   title,
   date,
-  // slug,
+  thumbnail,
+  slug,
   decription,
   category,
 }: PostItemType) => {
   return (
-    <article className={postArticle} role="link">
-      <div className={postTitleWrapper}>
-        <div>
-          <h2 className={postTitle}>{title}</h2>
+    <Link to={`/${slug}`}>
+      <article className={postArticle}>
+        <figure>
+          <GatsbyImage image={thumbnail!.gatsbyImageData!} alt={title!} />
+        </figure>
+        <div className={postTitleWrapper}>
+          <div>
+            <h2 className={postTitle}>{title}</h2>
+          </div>
+          <time>
+            <small className={postDate}>{formatDate(date!)}</small>
+          </time>
+          <p className={postDescription}>{decription?.decription}</p>
         </div>
         <div>
-          <small className={postDate}>
-            {new Date(date!).toLocaleDateString('ko-KR', {
-              year: 'numeric',
-              month: 'long',
-              day: 'numeric',
-            })}
-          </small>
-          {/* <p>{slug}</p> */}
+          {category?.map((item, index) => (
+            <small key={`${item}-${index}`} className={postCategory}>
+              {`#${item}`}
+            </small>
+          ))}
         </div>
-        <div className={postDescription}>{decription?.decription}</div>
-      </div>
-      <div>
-        {category?.map((item, index) => (
-          <small key={`${item}-${index}`} className={postCategory}>
-            {`#${item}`}
-          </small>
-        ))}
-      </div>
-    </article>
+      </article>
+    </Link>
   );
 };
 
