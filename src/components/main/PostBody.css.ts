13f958a123f0e0ee3871d28ca8411390e03cf9ee
jdy8739 <jdy8739@naver.com>
2025-04-05 21:34:26 +0900

    chore: set up PostBody structure

--- src/components/main/PostBody.css.ts
+++ src/components/main/PostBody.css.ts
@@ -1,1 +1,23 @@
-//
+import { style } from '@vanilla-extract/css';
+
+const postBody = style({
+  position: 'relative',
+  display: 'grid',
+  gridTemplateColumns: '1fr 220px',
+  gridGap: '30px',
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+  paddingTop: '100px',
+});
+
+const postBodyContent = style({
+  overflow: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '100px',
+  fontSize: '16px',
+  lineHeight: '2',
+  wordBreak: 'break-word',
+});
+
+export { postBody, postBodyContent };
