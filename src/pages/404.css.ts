12cb58000d20bb387e75419d60c35d33742b2e51
jdy8739 <jdy8739@naver.com>
2025-04-08 14:53:22 +0900

    feat: 404 page

---
+++ src/pages/404.css.ts
@@ -0,0 +1,45 @@
+import { style } from '@vanilla-extract/css';
+import { responsiveStyle } from '../styles/responseive.css';
+
+const notFoundContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  margin: '150px 0',
+  gap: '10px',
+});
+
+const notFoundTitle = style([
+  {
+    fontSize: '30px',
+    fontWeight: '700',
+  },
+  responsiveStyle({
+    tablet: {
+      fontSize: '25px',
+    },
+  }),
+]);
+
+const notFoundDescription = style([
+  {
+    marginTop: '10px',
+    fontSize: '20px',
+    fontWeight: '300',
+  },
+  responsiveStyle({
+    tablet: {
+      fontSize: '18px',
+    },
+  }),
+]);
+
+const notFoundLink = style([
+  {
+    marginTop: '30px',
+    fontSize: '15px',
+    color: 'inherit',
+  },
+]);
+
+export { notFoundContainer, notFoundTitle, notFoundDescription, notFoundLink };
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,25 @@
-import { HeadFC, PageProps } from 'gatsby';
+import { HeadFC, Link, PageProps } from 'gatsby';
+import {
+  notFoundContainer,
+  notFoundTitle,
+  notFoundDescription,
+  notFoundLink,
+} from './404.css';
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <main>
-      <h1>Page not found</h1>
+      <section className={notFoundContainer}>
+        <h1 className={notFoundTitle}>페이지를 찾을 수 없습니다.</h1>
+        <p className={notFoundDescription}>다른 콘텐츠를 보러 가보시겠어요?</p>
+        <Link to="/" className={notFoundLink}>
+          메인 페이지로
+        </Link>
+      </section>
     </main>
   );
 };
 
 export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>;
+export const Head: HeadFC = () => <title>Not found 404</title>;
