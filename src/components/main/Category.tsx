e5199f2037c014a685b90a8ded266f3154ac11d7
jdy8739 <jdy8739@naver.com>
2025-04-08 17:55:13 +0900

    feat: implement category filtering

--- src/components/main/Category.tsx
+++ src/components/main/Category.tsx
@@ -1,5 +1,34 @@
-const Category = () => {
-  return <div>Category</div>;
+import { useMemo } from 'react';
+
+type Props = {
+  category: string;
+  categoryMap: Map<string, number>;
+  handleCategoryChange: (category: string) => void;
+};
+
+const Category = ({ category, categoryMap, handleCategoryChange }: Props) => {
+  const categoryButtons = useMemo(() => {
+    return Array.from(categoryMap, ([name, count]) => ({ name, count }));
+  }, [categoryMap]);
+
+  return (
+    <section>
+      {categoryButtons.map(({ name, count }) => (
+        <button
+          key={name}
+          onClick={() => handleCategoryChange(name)}
+          style={{
+            backgroundColor:
+              category === name ? 'var(--primary)' : 'var(--background)',
+          }}
+        >
+          <span>{name}</span>
+          <span>-</span>
+          <span>{count}</span>
+        </button>
+      ))}
+    </section>
+  );
 };
 
 export default Category;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,59 @@
 import { graphql, type HeadFC, type PageProps } from 'gatsby';
 import '../styles/reset.css';
-import PostList from '../components/main/PostList';
+import PostList from '../components/post/PostList';
+import Introduction from '../components/main/Introduction';
+import Category from '../components/main/Category';
+import { useMemo, useState, useCallback } from 'react';
 
 export default function Index({
   data: {
     allContentfulBlogPost: { nodes: posts },
   },
 }: PageProps<Queries.IndexPageQuery>) {
+  const [category, setCategory] = useState('ALL');
+
+  const handleCategoryChange = useCallback((category: string) => {
+    setCategory(category);
+  }, []);
+
+  const categoryMap = useMemo(() => {
+    return posts.reduce(
+      (acc, cur) => {
+        if (cur.category) {
+          for (const category of cur.category) {
+            if (acc.has(category)) {
+              acc.set(category, acc.get(category) + 1);
+            } else {
+              acc.set(category, 1);
+            }
+          }
+        }
+
+        return acc;
+      },
+      new Map().set('ALL', posts.length),
+    );
+  }, [posts]);
+
+  const filteredPosts = useMemo(() => {
+    if (category === 'ALL') {
+      return posts;
+    }
+
+    return posts.filter(({ category: postCategory }) =>
+      postCategory?.includes(category),
+    );
+  }, [category, posts]);
+
   return (
     <>
-      <PostList posts={posts} />
+      <Introduction />
+      <Category
+        category={category}
+        categoryMap={categoryMap}
+        handleCategoryChange={handleCategoryChange}
+      />
+      <PostList posts={filteredPosts} />
     </>
   );
 }
