6a59c66d45cac9d2fbe483e74fbd51951fe34b45
jdy8739 <jdy8739@naver.com>
2025-04-08 14:10:28 +0900

    style: set display block in the classname postBody when mobile

--- src/components/main/PostBody.css.ts
+++ src/components/main/PostBody.css.ts
@@ -1,14 +1,23 @@
 import { style } from '@vanilla-extract/css';
+import { responsiveStyle } from '../../styles/responseive.css';
 
-const postBody = style({
-  position: 'relative',
-  display: 'grid',
-  gridTemplateColumns: '1fr 220px',
-  gridGap: '30px',
-  justifyContent: 'space-between',
-  alignItems: 'flex-start',
-  paddingTop: '100px',
-});
+const postBody = style([
+  {
+    position: 'relative',
+    display: 'grid',
+    gridTemplateColumns: '1fr 220px',
+    gridGap: '30px',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    paddingTop: '100px',
+  },
+  responsiveStyle({
+    mobile: {
+      display: 'block',
+      gridTemplateColumns: '',
+    },
+  }),
+]);
 
 const postBodyContent = style({
   overflow: 'auto',
