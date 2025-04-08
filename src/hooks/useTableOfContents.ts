b1ce425f00689709d1fc0a3cac19547a2d8d788f
jdy8739 <jdy8739@naver.com>
2025-04-08 13:35:07 +0900

    refac: modify the enrolling part to a function in useTableContents

--- src/hooks/useTableOfContents.ts
+++ src/hooks/useTableOfContents.ts
@@ -1,8 +1,9 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 const useTableContents = (rawContent: string) => {
   const [activeId, setActiveId] = useState<string | null>(null);
 
+  /** raw content의 헤더요소에서 추출한 테이블 오브 콘텐츠 리스트 */
   const tableOfContents = useMemo<
     {
       title: string;
@@ -12,10 +13,12 @@ const useTableContents = (rawContent: string) => {
   >(() => {
     const { content } = JSON.parse(rawContent);
 
+    /** 컨텐츠 내용에서 타이틀 요소 리스트 */
     const headings = content.filter(({ nodeType }: { nodeType: string }) =>
       nodeType.includes('heading'),
     );
 
+    /** 컨텐츠 내용에서 타이틀 요소 중 가장 큰 헤더 태그 레벨 */
     const minDepth = Math.min(
       ...headings.map(({ nodeType }: { nodeType: string }) =>
         Number(nodeType.charAt(nodeType.length - 1)),
@@ -41,31 +44,46 @@ const useTableContents = (rawContent: string) => {
     );
   }, [rawContent]);
 
+  /** 관측할 타이틀 요소를 옵저버에 등록하는 함수 */
+  const enrollObserver = useCallback(
+    ({
+      query,
+      callback,
+    }: {
+      query: string;
+      callback: (element: Element) => void;
+    }) => document.querySelectorAll(query).forEach(callback),
+    [],
+  );
+
   useEffect(() => {
     const observer = new IntersectionObserver(
-      entries =>
-        setActiveId(prevId => {
-          if (entries[0].boundingClientRect.top < 0) {
+      ([firstEntry]) => {
+        if (firstEntry) {
+          setActiveId(prevActiveId => {
             // 스크롤을 아래로 내리는 경우
-            return entries[0].target.id;
-          } else {
-            // 스크롤을 위로 올리는 경우
-            const index = tableOfContents.findIndex(({ id }) => id === prevId);
+            if (firstEntry.boundingClientRect.top < 0) {
+              return firstEntry.target.id;
+            }
 
-            return index > 0 ? tableOfContents[index - 1].id : null;
-          }
-        }),
+            const activeIndex = tableOfContents.findIndex(
+              ({ id }) => id === prevActiveId,
+            );
+
+            return activeIndex > 0 ? tableOfContents[activeIndex - 1].id : null;
+          });
+        }
+      },
       { rootMargin: '0% 0px -100% 0px' },
     );
 
-    document
-      .querySelectorAll('#content > h1, #content > h2, #content >h3')
-      .forEach(element => observer.observe(element));
+    enrollObserver({
+      query: '#content > h1, #content > h2, #content >h3',
+      callback: (element: Element) => observer.observe(element),
+    });
 
     return () => observer.disconnect();
   }, [tableOfContents]);
-
-  console.log(activeId);
 
   return { activeId, tableOfContents };
 };
