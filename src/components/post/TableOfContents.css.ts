57ffb36c9b2ba9055446b80fce4644548a2d9c3a
jdy8739 <jdy8739@naver.com>
2025-04-08 14:10:42 +0900

    style: set display none in the classname tocContainer when mobile

--- src/components/post/TableOfContents.css.ts
+++ src/components/post/TableOfContents.css.ts
@@ -1,13 +1,21 @@
 import { style } from '@vanilla-extract/css';
+import { responsiveStyle } from '../../styles/responseive.css';
 
-const tocContainer = style({
-  position: 'sticky',
-  backgroundColor: '#f0f0f0',
-  padding: '15px',
-  borderRadius: '10px',
-  top: '100px',
-  width: '100%',
-});
+const tocContainer = style([
+  {
+    position: 'sticky',
+    backgroundColor: '#f0f0f0',
+    padding: '15px',
+    borderRadius: '10px',
+    top: '100px',
+    width: '100%',
+  },
+  responsiveStyle({
+    mobile: {
+      display: 'none',
+    },
+  }),
+]);
 
 const tocTitle = style({
   paddingBottom: '20px',
