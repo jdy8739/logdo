801eac9d158690cf8131880a42e88c9edac4ccdc
jdy8739 <jdy8739@naver.com>
2025-04-09 20:28:18 +0900

    feat: setup SEO component

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,35 +1,36 @@
-import type { GatsbyConfig } from "gatsby";
-import dotenv from "dotenv";
+import type { GatsbyConfig } from 'gatsby';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `logdo`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `여기에 원하는 제목을 추가해주세요!`,
+    description: '여기에 원하는 설명을 추가해주세요!',
+    siteUrl: `https://naver.com`, // 대괄호 부분에 깃허브 유저명을 넣어주세요.
   },
-  jsxRuntime: "automatic",
+  jsxRuntime: 'automatic',
   graphqlTypegen: true,
   plugins: [
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
         spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-vanilla-extract",
-    "gatsby-plugin-sitemap",
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-vanilla-extract',
+    'gatsby-plugin-sitemap',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
