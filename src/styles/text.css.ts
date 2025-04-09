ec628c85ad7a8b600f73da26e3a20e4d8f0818c8
jdy8739 <jdy8739@naver.com>
2025-04-10 01:42:40 +0900

    chore: create a file for globalFontFace

---
+++ src/styles/text.css.ts
@@ -0,0 +1,14 @@
+// import {
+//     globalFontFace,
+//     style
+//   } from '@vanilla-extract/css';
+
+//   const comicSans = 'GlobalComicSans';
+
+//   globalFontFace(comicSans, {
+//     src: 'local("Comic Sans MS")'
+//   });
+
+//   export const font = style({
+//     fontFamily: comicSans
+//   });
