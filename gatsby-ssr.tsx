b1979ac4dc0cc419de60fab42555f4c440bc8644
jdy8739 <jdy8739@naver.com>
2025-10-04 11:51:37 +0900

    feat: self-host Pretendard variable font for security and performance
    - Download and serve PretendardVariable.woff2 from /static/fonts/
    - Remove CDN dependency (jsdelivr) to eliminate security risk
    - Add font-face declaration in src/styles/fonts.css
    - Update font-family stack with fallbacks
    - Benefits: better performance, privacy, and security

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -2,18 +2,6 @@ import { GatsbySSR } from 'gatsby';
 import Layout from './src/components/common/layout/Layout';
 import { ThemeProvider } from './src/contexts/ThemeContext';
 
-const onRenderBody: GatsbySSR['onRenderBody'] = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <link
-      rel="stylesheet"
-      as="style"
-      crossOrigin="anonymous"
-      href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css"
-      key="pretendard-font"
-    />,
-  ]);
-};
-
 const wrapPageElement: GatsbySSR['wrapPageElement'] = ({
   element,
 }: {
@@ -26,4 +14,4 @@ const wrapPageElement: GatsbySSR['wrapPageElement'] = ({
   );
 };
 
-export { wrapPageElement, onRenderBody };
+export { wrapPageElement };
